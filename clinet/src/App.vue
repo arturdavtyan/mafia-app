aa31c5ebc7e0d9994f937a7b79c6395d3d9ba85e
Artur Davtyan <ad220897@gmail.com>
2020-11-30 15:00:24 +0400

    Initial config

---
+++ clinet/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1 style="width: 200px; background: lightgreen; word-break: break-all">Header: Hello World</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created() {
+    this.SetWindowWidth()
+    window.addEventListener('resize', this.SetWindowWidth)
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.SetWindowWidth)
+  },
+  methods: {
+    SetWindowWidth() {
+      this.$store.commit('General/SetWindowWidth', window.innerWidth)
+    }
+  }
+}
+</script>
